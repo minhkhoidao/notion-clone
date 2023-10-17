4fe9c56a60b595d3522ea1f65034a5bc3570d787
daominhkhoi <108728247+minhkhoidao@users.noreply.github.com>
2023-10-17 15:35:21 +0700

    feature: init database convex, api create, get, and implement api (#1)

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,28 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create new note",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -23,9 +39,11 @@ const DocumentsPage = () => {
         alt="empty"
         className="hidden dark:block"
       />
-      <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s Jotion</h2>
-      <Button>
-        <PlusCircle className="h-4 w-4 mr-2"/>
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
     </div>
